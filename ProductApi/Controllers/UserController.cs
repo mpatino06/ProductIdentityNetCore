f092cf086734a9575b3f267ff86d67a91aa82c8d
Miguel Patino <mpatino06@hotmail.com>
2021-04-09 17:22:35 -0500

    Servicio cognitivo de Azure Translator

---
+++ ProductApi/Controllers/UserController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using ProductApi.Models.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> userManager;
+        private readonly SignInManager<IdentityUser> signInManager;
+        private readonly RoleManager<IdentityRole> roleManager;
+
+        public UserController(UserManager<IdentityUser> userManager, 
+            SignInManager<IdentityUser> signInManager, RoleManager<IdentityRole> roleManager )
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+            this.roleManager = roleManager;
+        }
+
+
+        [HttpPost("Register")]
+        public async Task<IActionResult> Register([FromBody] UserDTO userDTO) {
+
+            var user = new IdentityUser { UserName = userDTO.Email, Email = userDTO.Email };
+
+            var result = await userManager.CreateAsync(user, userDTO.Password);
+            if (result.Succeeded)
+            {
+                if (!await roleManager.RoleExistsAsync("Admin"))
+                {
+                    await roleManager.CreateAsync(new IdentityRole("Admin"));
+                }
+                if (!await roleManager.RoleExistsAsync("Customer"))
+                {
+                    await roleManager.CreateAsync(new IdentityRole("Customer"));
+                }
+                await userManager.AddToRoleAsync(user, "Customer");
+
+                return NoContent();
+            }
+
+            foreach (var item in result.Errors)
+            {
+                ModelState.AddModelError("Response", item.Description);
+            }
+            return StatusCode(400, ModelState);
+        
+        }
+    }
+}
